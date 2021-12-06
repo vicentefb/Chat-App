1f7bc2dc4f31663308785d2ece13fc82ec1f322e
vicentefb <chenteferrara@gmail.com>
2021-12-06 12:21:45 -0600

    signup screen

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { Context } from "../context";
+
+import { useRouter } from "next/router";
+
+import axios from "axios";
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  const { setUsername, setSecret } = useContext(Context);
+
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={(e) => e.preventDefault()}>
+          <div className="auth-title">NextJS Chat</div>
+
+          <div classNmae="input-container">
+            <input
+              placeholder="Email"
+              className="text-input"
+              onChange={(e) => setUsername(e.target.value)}
+            ></input>
+          </div>
+
+          <div classNmae="input-container">
+            <input
+              type="password"
+              placeholder="Password"
+              className="text-input"
+              onChange={(e) => setSecret(e.target.value)}
+            ></input>
+          </div>
+          <button type="submit" className="submit-button">
+            Login / Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
